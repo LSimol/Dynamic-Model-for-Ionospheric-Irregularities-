a3ee5fd209f9074fe21c28126d01a5f80b282ea8
LSimol <150791857+LSimol@users.noreply.github.com>
2025-02-20 00:40:40 -0900

    Add files via upload
    Libraries required by the function

---
+++ _init_.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import healpy as hp
+from healpy.newvisufunc import projview, newprojplot
+import matplotlib.colors
+from matplotlib.ticker import (MultipleLocator, AutoLocator)
+from scipy.interpolate import interp1d
